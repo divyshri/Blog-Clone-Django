73e1a047216e0de6c5e491c42c54373504b5bca2
divyshri <divyanshu4shrivastava@gmail.com>
2020-06-29 18:52:00 +0530

    Views Implemented

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     url(r'post/(?P<pk>\d+)/edit/$',view.PostUpdateView.as_view(),name='post_edit'),
     url(r'^post/(?P<pk>\d+)/remove/$',views.PostDetailView.as_view(),name='post_remove'),
     url(r'^post/drafts/$',views.DraftListView.as_view(),name='post_draft_list'),
+    url(r'^post/(?P<pk>\d+)/comment/$'views.add_comments_to_post,name='add_comments_to_post'),
+    url(r'^comment/(?P<pk>\d+)/approve/$',views.comment_approve,name='comment_approve'),
+    url(r'^comment/(?P<pk>\d+)/remove/$',views.comment_remove,name='comment_remove'),
+    url(r'^post/(?P<pk>\d+)/publish/$',views.post_publish,name='post_publish'),
+
 ]
